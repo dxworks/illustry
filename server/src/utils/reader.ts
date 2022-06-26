81fa362fe029a7143af155bf8f361462db190651
vladimirnitu <50229186+vladimirnitu@users.noreply.github.com>
2022-06-26 01:19:09 -0700

    removing the in memory processing of the files and using streams

--- server/src/utils/reader.ts
+++ server/src/utils/reader.ts
@@ -2,38 +2,38 @@ import _ from "lodash";
 import { Promise } from "bluebird";
 import { FileError } from "../errors/fileError";
 import { FileProperties } from "types/fileproperties";
-// const converter = require('json-2-csv')
-var fs = require('fs')
+var fs = require("fs");
 
 const read = (file: FileProperties) => {
+  if (file.type === "application/json") {
     return new Promise((resolve, reject) => {
-        fs.readFile(_.get(file, 'filePath'), (error: any, data: any) => {
-            if (error) {
-                reject(new FileError("Problems while uploading the files"))
-            }
-            if (file.type === 'application/json') {
-                let illustration = JSON.parse(data.toString());
-                let finalJson: any = {
-                    data: illustration?.data,
-                    name: illustration?.name,
-                    description:illustration?.description,
-                    type: illustration?.type,
-                    tags: illustration?.tags
-                }
-
-                resolve(finalJson)
-            }
-        })
-    })
-}
+      const buffer = fs.createReadStream(_.get(file, "filePath"), {
+        encoding: "utf8",
+      });
+      let finalText: string = "";
+      buffer.on("error", (err: any) => {
+        reject(new FileError("Problems while uploading the files"));
+      });
+      buffer.on("data", (data: any) => {
+        finalText = finalText + data;
+      });
+      buffer.on("end", () => {
+        fs.unlinkSync(_.get(file, "filePath"));
+        let illustration = JSON.parse(finalText);
+        let finalJson: any = {
+          data: illustration?.data,
+          name: illustration?.name,
+          description: illustration?.description,
+          type: illustration?.type,
+          tags: illustration?.tags,
+        };
+        resolve(finalJson);
+      });
+    });
+  }
+};
 export const readFile = (files: FileProperties[]) => {
-    return Promise.all(files.map(read))
-        .then((files) => {
-            return files
-
-        })
-
-
-
-}
- 
+  return Promise.all(files.map(read)).then((files) => {
+    return files;
+  });
+};
